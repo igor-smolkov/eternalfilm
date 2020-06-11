c13f251f53615fc02bb4ff89317920adae7c0b76
Igor Smolkov <smolkov.igor@gmail.com>
2020-06-12 01:33:49 +0700

    Change frame

--- src/script.js
+++ src/script.js
@@ -1,20 +1,29 @@
 import '@/style.scss'
 
 const link = '2cfnex';
-const timer = 5000;
+const timer = 10000;
+const loadDiff = 500;
 
 let screen = document.querySelector('.screen');
 screen.addEventListener('click', function(){
+    loadCoub();
     show();
 });
 
-function show(){
+function show() {
     screen.firstChild.remove();
-    screen.append(createCoub(link));
+    screen.firstChild.display = 'block';
+    setTimeout(loadCoub, timer-loadDiff);
     setTimeout(show, timer);
-}
+};
 
-function createCoub(link){
+function loadCoub() {
+    let coub = createCoub(link);
+    coub.style.display = 'none';
+    screen.append(createCoub(link));
+};
+
+function createCoub(link) {
     let coub = document.createElement('iframe');
     coub.className = 'coub';
     coub.src = 'https://coub.com/embed/'+link+'?muted=false&autostart=true&originalSize=false&startWithHD=false';
