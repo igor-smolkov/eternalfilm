5608624d9829dc8eef4ad92bcf5a63fabfcebaa5
Igor Smolkov <smolkov.igor@gmail.com>
2020-06-12 00:36:46 +0700

    Add timer and coub pos

--- src/script.js
+++ src/script.js
@@ -1,18 +1,26 @@
 import '@/style.scss'
 
-function createCoub(){
+const link = '2cfnex';
+const timer = 5000;
+
+let screen = document.querySelector('.screen');
+screen.addEventListener('click', function(){
+    show();
+});
+
+function show(){
+    screen.firstChild.remove();
+    screen.append(createCoub(link));
+    setTimeout(show, timer);
+}
+
+function createCoub(link){
     let coub = document.createElement('iframe');
     coub.className = 'coub';
-    coub.src = 'https://coub.com/embed/2cfnex?muted=false&autostart=true&originalSize=false&startWithHD=false';
+    coub.src = 'https://coub.com/embed/'+link+'?muted=false&autostart=true&originalSize=false&startWithHD=false';
     coub.frameBorder = 0;
     coub.width = '100%';
     coub.height = '100%';
     coub.allow = 'autoplay';
     return coub;
-}
-
-let screen = document.querySelector('.screen');
-screen.addEventListener('click', function(){
-    screen.firstChild.remove();
-    screen.append(createCoub());
-});
+};
