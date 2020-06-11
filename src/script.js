522180da18ca6898e2e92526cc3b77657f7a071d
Igor Smolkov <smolkov.igor@gmail.com>
2020-06-12 00:19:07 +0700

    Add coub onclick screen

--- src/script.js
+++ src/script.js
@@ -1,2 +1,18 @@
 import '@/style.scss'
-console.log('script here');
+
+function createCoub(){
+    let coub = document.createElement('iframe');
+    coub.className = 'coub';
+    coub.src = 'https://coub.com/embed/2cfnex?muted=false&autostart=true&originalSize=false&startWithHD=false';
+    coub.frameBorder = 0;
+    coub.width = '100%';
+    coub.height = '100%';
+    coub.allow = 'autoplay';
+    return coub;
+}
+
+let screen = document.querySelector('.screen');
+screen.addEventListener('click', function(){
+    screen.firstChild.remove();
+    screen.append(createCoub());
+});
