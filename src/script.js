b4de1cb858c87e7904a60649e7ef674dfb7b8786
Igor Smolkov <smolkov.igor@gmail.com>
2020-06-15 01:29:29 +0700

    Change coub concept on youtube

--- src/script.js
+++ src/script.js
@@ -3,25 +3,75 @@ import '@/style.scss'
 import baseJSON from '@static/base.json'
 
 let base = baseJSON.links;
-const timer = 10000;
+const timer = 5000;
 const loadDiff = 500;
 
-$.ajax({
-    url: 'getBase.php',
-    dataType: 'json',
-    cache: false,
-    data: '',
-    type: 'post',
-    success: function(php_script_response){
-        base = php_script_response.links;
-    }
-});
+//connect();
+function connect() {
+    $.ajax({
+        url: 'getBase.php',
+        dataType: 'json',
+        cache: false,
+        data: '',
+        type: 'post',
+        success: function(php_script_response){
+            base = php_script_response.links;
+        }
+    });
+}
+
+loadYTApi();
+function loadYTApi() {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
 
 let screen = document.querySelector('.screen');
 screen.addEventListener('click', function(){
-    loadCoub();
-    show();
+    screen.firstChild.style.display = 'block';
+    screen.lastChild.remove();
+    onYouTubeIframeAPIReady();
+    //show();
 });
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: randLink(),
+        playerVars: { 'autoplay': 1, 'controls': 0 },
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        const randStart = randN(0,player.getDuration()-(timer/1000));
+        player.seekTo(randStart);
+        setTimeout(stopVideo, timer);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+    playVideo();
+}
+
+function playVideo() {
+    player.cueVideoById(randLink(), 10);
+    player.playVideo();
+    done = false;
+}
 
 let addBtn = document.getElementById('add');
 addBtn.addEventListener('click', function(){
@@ -45,27 +95,6 @@ addBtn.addEventListener('click', function(){
 
     base.push(link);
 });
-
-function show() {
-    screen.firstChild.remove();
-    screen.firstChild.classList.toggle('coub_hidden');
-    setTimeout(loadCoub, timer-loadDiff);
-    setTimeout(show, timer);
-};
-
-function loadCoub() {
-    let link = randLink();
-    let coub = createCoub(link);
-    screen.append(createCoub(link));
-};
-
-function createCoub(link) {
-    let coub = document.createElement('iframe');
-    coub.className = 'coub coub_hidden';
-    coub.src = 'https://coub.com/embed/'+link+'?muted=false&autostart=true&originalSize=false&startWithHD=false';
-    coub.allow = 'autoplay';
-    return coub;
-};
 
 function randLink() {
     return base[randN(0,base.length)];
