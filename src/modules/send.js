03189d4696ab0cf37527d2ed7b6b7f94a639e4db
Igor Smolkov <smolkov.igor@gmail.com>
2020-06-22 18:37:46 +0700

    Changed send module, jquery delited

--- src/modules/send.js
+++ src/modules/send.js
@@ -1,14 +1,12 @@
-import * as $ from 'jquery'
-
 export default function(link) {
-    $.ajax({
-        url: 'setBase.php',
-        dataType: 'text',
-        cache: false,
-        data: ({str: link}),
-        type: 'post',
-        success: function(php_script_response){
-            console.log(php_script_response)
+    const body = 'str=' + encodeURIComponent(link);
+    const request = new XMLHttpRequest();
+    request.open('POST','setBase.php',true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.addEventListener('readystatechange', function() {
+        if ((request.readyState==4) && (request.status==200)) {
+            console.log(request.responseText);
         }
     });
+    request.send(body);
 }
